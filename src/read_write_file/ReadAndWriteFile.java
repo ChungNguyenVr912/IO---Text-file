fb69509d067825e4485cdff26203b527640660bd
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-20 09:01:25 +0700

    Update ReadAndWriteFile.java

--- src/read_write_file/ReadAndWriteFile.java
+++ src/read_write_file/ReadAndWriteFile.java
@@ -6,32 +6,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReadAndWriteFile {
     public static void main(String[] args) {
-        File file = new File("src/read_write_file/numbers.txt");
+        String inputUrl = "src/read_write_file/numbers.txt";
         File outputFile = new File("src/read_write_file/result.txt");
         try {
-            ArrayList<Integer> arrayList =  readFile(file);
+            ArrayList<Integer> arrayList = readFile(inputUrl);
             AtomicInteger max = new AtomicInteger(arrayList.get(0));
-            arrayList.forEach(number -> {
-                max.set(Math.max(max.get(), number));
-            });
+            arrayList.forEach(number ->
+                    max.set(Math.max(max.get(), number))
+            );
             String result = "Max value is: " + max;
             writeFile(outputFile, result);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void writeFile(File outputFile, String result) throws IOException{
+    private static void writeFile(File outputFile, String result) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
         writer.write(result);
         writer.close();
     }
 
-    private static ArrayList<Integer> readFile(File file) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+    private static ArrayList<Integer> readFile(String inputUrl) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(inputUrl));
         ArrayList<Integer> arrayList = new ArrayList<>();
         String line;
-        while((line = reader.readLine()) != null){
+        while ((line = reader.readLine()) != null) {
             arrayList.add(Integer.parseInt(line));
         }
         reader.close();
